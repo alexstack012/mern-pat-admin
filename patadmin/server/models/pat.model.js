f9c76a5e71e3126ce74ae2ac780e5faad9239cbd
Alex Stack <alex_stack012@live.com>
2022-10-04 13:08:20 -0400

    updated model

--- patadmin/server/models/pat.model.js
+++ patadmin/server/models/pat.model.js
@@ -1,7 +1,16 @@
 const mongoose = require('mongoose')
 const PatSchema = new mongoose.Schema({
     name: { type: String },
-    insurance: { type: String },
-    notes: [{ type: String}],
+    notes: [{ type: String }],
+    meds: [{ type: String }],
+    allergies: [{ type: String }],
+    dob: { type: String },
+    religion: { type: String },
+    height: { type: String },
+    weight: { type: Number },
+    insurName:{ type: String },
+    insurMemId:{ type: String },
+    insurGroupId:{ type: String },
+    lastVisit:{ type: String },
 }, { timestamps: true });
 module.exports.Pat = mongoose.model('Pat', PatSchema);
